0147fb1a34fcac56160df31d01bad7e1d8c0e531
shrey <shreysgupta@gmail.com>
2023-03-03 14:17:31 -0800

    basic ui for mvp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,47 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import styles from '../styles.module.css'
+import Head from 'next/head';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+import { useState } from 'react'
 
-export default IndexPage
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [courses, setCourses] = useState([])
+
+  const handleSearch = async () => {
+    const response = await fetch('/api/search', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: searchTerm })
+    })
+    const data = await response.json()
+    setCourses(data)
+  }
+
+  return (
+    <div className={styles['body']}>
+      <Head>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+</Head>
+
+      <input type="text" className={styles['search-input']} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <button className={styles['button']} onClick={handleSearch}>Search</button>
+      <div>
+      <div className={styles.search}>
+        {courses.map((course) => (
+          <div key={course.id} className={styles.card}>
+            <h3>{course.data.title}</h3>
+            <p>{course.data.description}</p>
+            <p>{course.data.units} units</p>
+          </div>
+        ))}
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Search
+
+// export default IndexPage
